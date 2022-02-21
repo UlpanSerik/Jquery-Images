333beb94a0f17bf15909a3790d5aca8b27f4d431
UlpanSerik <bagitzanovaulpan@gmail.com>
2022-02-21 12:10:29 +0600

    first commit

---
+++ Assets/scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#saturn').on('click', function(){
+        $('.planet_img img').attr('src', 'Assets/img/saturn.jpg')
+    })
+
+    $('#mercury').on('click', function(){
+        $('.planet_img img').attr('src', 'Assets/img/mercury.jpg')
+    })
+
+    $('#earth').on('click', function(){
+        $('.planet_img img').attr('src', 'Assets/img/earth.jpg')
+    })
+
+})
